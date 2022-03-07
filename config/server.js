58e3bdc64d342206d1373ea9a44f96f173cac76d
Sumit Jain <sumit.diversity@gmail.com>
2022-03-07 22:28:33 +0530

    newgit

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: 'https://strapi4version.newweb.tngwebsolutions.ca/'
+  url: 'https://strapi4version.newweb.tngwebsolutions.ca/',
   app: {
     keys: env.array('APP_KEYS'),
   },
