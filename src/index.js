f70e0864cd35d02fe2b3d2743ca9671264f60ef4
Sumit Jain <sumit.diversity@gmail.com>
2022-03-07 22:39:03 +0530

    newgit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,27 @@
+// 'use strict';
+
+// module.exports = {
+//   /**
+//    * An asynchronous register function that runs before
+//    * your application is initialized.
+//    *
+//    * This gives you an opportunity to extend code.
+//    */
+//   register(/*{ strapi }*/) {},
+
+//   /**
+//    * An asynchronous bootstrap function that runs before
+//    * your application gets started.
+//    *
+//    * This gives you an opportunity to set up your data model,
+//    * run jobs, or perform some special logic.
+//    */
+//   bootstrap(/*{ strapi }*/) {},
+// };
+
 'use strict';
+
+const session = require("koa-session2");
 
 module.exports = {
   /**
@@ -7,7 +30,11 @@ module.exports = {
    *
    * This gives you an opportunity to extend code.
    */
-  register(/*{ strapi }*/) {},
+  register({ strapi }) {
+    strapi.server.use(session({
+      secret: "grant",
+    }));
+  },
 
   /**
    * An asynchronous bootstrap function that runs before
@@ -16,5 +43,5 @@ module.exports = {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/*{ strapi }*/) {},
+  bootstrap(/*{ strapi }*/) { },
 };
