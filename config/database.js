b87a71e48173e45fd8cb9eca4a0c860da28ef373
Baljot Gill <32570391+baljotgill@users.noreply.github.com>
2022-03-11 10:15:21 +0530

    first commit

--- config/database.js
+++ config/database.js
@@ -2,11 +2,12 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
-      host: env('DATABASE_HOST', '185.214.10.151'),
-      port: env.int('DATABASE_PORT', 1002),
+      host: env('DATABASE_HOST', 'localhost'),
+      port: env.int('DATABASE_PORT', 3306),
       database: env('DATABASE_NAME', 'stripidb'),
       user: env('DATABASE_USERNAME', 'root'),
-      password: env('DATABASE_PASSWORD', 'Iamnumber!23'),
+      // password: env('DATABASE_PASSWORD', 'Iamnumber!23'),
+      password: env('DATABASE_PASSWORD', ''),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
